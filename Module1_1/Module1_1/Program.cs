6832ace34c8a076eeefd212d288b7302b0804b7e
Alexandr Usovich <l0sty@yandex.ru>
2019-10-10 23:34:07 +0300

    New project created Task completed

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = a;
+
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
