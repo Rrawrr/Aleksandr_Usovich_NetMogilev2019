2795dccb9f5b2517eae65bfd9240e044c2df874f
Alexandr Usovich <l0sty@yandex.ru>
2019-10-30 21:43:51 +0300

    The last Task in Module3 has been completed

---
+++ Module3_8_1/Module3_8_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace Module3_8_1
+{
+    class Program
+    {
+
+        static double Function(double x)
+        {
+            return Math.Pow(x, 3) - 8;
+        }
+        static void Main(string[] args)
+        {
+            double iteration = 0, functionStart, functionEnd, functionIteration;
+            double epsilon = 0.001;
+            int divisionsCounter = 0;
+
+            Console.WriteLine("Your function is: f(x) = x ^ 3 - 8");
+            Console.WriteLine();
+
+            Console.Write("Enter the start of the segment: ");
+            double start = InputChecker(Console.ReadLine());
+
+            Console.Write("Enter the end of the segment: ");
+            double end = InputChecker(Console.ReadLine());
+           
+            functionStart = Function(start);
+            functionEnd = Function(end);
+
+            if (functionStart * functionEnd > 0)
+            {
+                Console.WriteLine("There is no solution on the entered segment");
+                Console.ReadLine(); 
+            }
+
+            if (Math.Abs(functionStart) < epsilon)
+            {
+                Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", start, divisionsCounter);
+                Console.ReadLine();
+            }
+
+            if (Math.Abs(functionEnd) < epsilon)
+            {
+                Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", end, divisionsCounter);
+                Console.ReadLine();
+            }
+
+            do
+            {
+                iteration = start + (end - start)/2;
+                functionIteration = Function(iteration);
+                divisionsCounter++;
+
+                if (Math.Abs(functionIteration) < epsilon)
+                    break;
+                if (functionStart * functionIteration < 0)
+                    end = iteration;
+                else
+                    start = iteration;
+            }
+            while (Math.Abs(start - end) > epsilon);
+
+            Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", iteration, divisionsCounter);
+            Console.ReadLine();
+        }
+
+        static double InputChecker(string input)
+        {
+            if (double.TryParse(input, out double parsedInput))
+            {
+                return parsedInput;
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct data!");
+                string newInput = Console.ReadLine();
+                return InputChecker(newInput);
+            }
+        }
+    }
+}
---
+++ Module3_8_2/Module3_8_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Module3_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the size of your array:");
+            int size = InputChecker(Console.ReadLine());
+            int[,] array = new int[size, size];
+            int currentNumber = 1;
+
+            for (int offset = 0; offset < size - 2; offset++)
+            {
+
+                for (int columnCounter = offset; columnCounter < size - offset; columnCounter++)
+                {
+                    array[offset, columnCounter] = currentNumber;
+                    currentNumber++;
+                }
+
+                for (int rowCounter = 1 + offset; rowCounter < size - offset; rowCounter++)
+                {
+                    array[rowCounter, size - (1 + offset)] = currentNumber;
+                    currentNumber++;
+                }
+
+                for (int columnCounter = size - (2 + offset); columnCounter >= offset; columnCounter--)
+                {
+                    array[size - (1 + offset), columnCounter] = currentNumber;
+                    currentNumber++;
+                }
+
+                for (int rowCounter = size - (2 + offset); rowCounter > offset; rowCounter--)
+                {
+                    array[rowCounter, offset] = currentNumber;
+                    currentNumber++;
+                }
+            }
+
+            ShowArray(array, size);
+
+        }
+
+        static void ShowArray(int[,] array, int size)
+        {
+            for (int rowCounter = 0; rowCounter < size; rowCounter++)
+            {
+                for (int columnCounter = 0; columnCounter < size; columnCounter++)
+                {
+                    Console.Write(string.Format("{0,3}", array[rowCounter, columnCounter]));
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+
+        static int InputChecker(string input)
+        {
+            if (int.TryParse(input, out int parsedInput) && parsedInput > 0)
+            {
+                return parsedInput;
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct data!");
+                string newInput = Console.ReadLine();
+                return InputChecker(newInput);
+            }
+        }
+    }
+}
