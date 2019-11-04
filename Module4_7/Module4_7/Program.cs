4572444b69ed38395bbf9ad11cb5e2f78cdd2ef5
Alexandr Usovich <l0sty@yandex.ru>
2019-11-04 21:57:58 +0300

    Module4 Tasks 5-8 were done

---
+++ Module4_7/Module4_7/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+namespace Module4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array");
+            int arrayLength = InputChecker(Console.ReadLine());
+            int[] inputArray = RandomNumbers(arrayLength);
+
+            Console.WriteLine("Enter the direction of sorting: + for increasing - for decreasing");
+            bool direction = DirectionChecker(Console.ReadLine());
+
+            Console.WriteLine("Sorted array is: ");
+            SortArray(inputArray, direction);
+
+            Console.WriteLine(string.Join(" ", inputArray));
+            Console.ReadLine();
+        }
+
+        static void SortArray(int[] inputArray,bool direction)
+        {
+            if (direction)
+                Array.Sort(inputArray);
+            if (!direction)
+            {
+                Array.Sort(inputArray);
+                Array.Reverse(inputArray);
+            }
+        }
+
+        static int[] RandomNumbers(int arrayLength)
+        {
+            int[] numbers = new int[arrayLength];
+            Random rnd = new Random();
+
+            for (int counter = 0; counter < arrayLength; counter++)
+            {
+                numbers[counter] = rnd.Next(-100, 100);
+            }
+            Console.WriteLine("Your array is:");
+            Console.WriteLine(string.Join(" ", numbers));
+
+            return numbers;
+        }
+
+        static bool DirectionChecker(string direction)
+        {
+            switch (direction)
+            {
+                case "+":
+                    return true;
+                case "-":
+                    return false;
+                default:
+                    Console.WriteLine("Enter the correct data! +/-");
+                    string newInput = Console.ReadLine();
+                    return DirectionChecker(newInput);
+            }
+                
+        }
+
+        static int InputChecker(string input)
+        {
+            if (int.TryParse(input, out int parsedInput) && parsedInput > 0)
+            {
+                return parsedInput;
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct data!");
+                string newInput = Console.ReadLine();
+                return InputChecker(newInput);
+            }
+        }
+    }
+}
---
+++ Module4_8/Module4_8/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Module4_8
+{
+    class Program
+    {
+        static double iteration = 0, functionStart, functionEnd, functionIteration;
+        static readonly double epsilon = 0.001;
+        static int divisionsCounter = 0;
+
+        static double Function(double x)
+        {
+            return Math.Pow(x, 3) - 8;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your function is: f(x) = x ^ 3 - 8");
+            Console.WriteLine();
+
+            Console.Write("Enter the start of the segment: ");
+            double start = InputChecker(Console.ReadLine());
+
+            Console.Write("Enter the end of the segment: ");
+            double end = InputChecker(Console.ReadLine());
+
+            functionStart = Function(start);
+            functionEnd = Function(end);
+
+            if (functionStart * functionEnd > 0)
+            {
+                Console.WriteLine("There is no solution on the entered segment");
+                Console.ReadLine();
+            }
+            else if (Math.Abs(functionStart) < epsilon)
+            {
+                Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", start, divisionsCounter);
+                Console.ReadLine();
+            }
+            else if (Math.Abs(functionEnd) < epsilon)
+            {
+                Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", end, divisionsCounter);
+                Console.ReadLine();
+            }
+
+            BisectionMethod(start,end);
+            Console.WriteLine("Found solution x = {0}, by N = {1} divisions!", iteration, divisionsCounter);
+            Console.ReadLine();
+
+        }
+
+        static (double,int) BisectionMethod(double start,double end)
+        {
+
+            iteration = (start + end) / 2;
+            functionIteration = Function(iteration);
+
+            if (Math.Abs(functionIteration) <= epsilon)
+            {
+                return (iteration, divisionsCounter);
+            }
+            else
+            {
+                if (functionStart * functionIteration < 0)
+                {
+                    end = iteration;
+                    divisionsCounter++;
+                }
+                if (functionEnd * functionIteration < 0)
+                {
+                    start = iteration;
+                    divisionsCounter++;
+                }
+                return BisectionMethod(start, end);
+            }
+        }
+
+        static double InputChecker(string input)
+        {
+            if (double.TryParse(input, out double parsedInput))
+            {
+                return parsedInput;
+            }
+            else
+            {
+                Console.WriteLine("Enter the correct data!");
+                string newInput = Console.ReadLine();
+                return InputChecker(newInput);
+            }
+        }
+    }
+}
